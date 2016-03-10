61a79e89b7440f2a7027514bf506eaff1f206126
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-03-10 18:36:04 +0700

    app path
    tambaha app path dan format.

--- u_dm.pas
+++ u_dm.pas
@@ -3,9 +3,9 @@ unit u_dm;
 interface
 
 uses
-  SysUtils, Classes, mySQLDbTables, DB, sSkinManager, dialogs,forms,
-  ImgList, Controls, acAlphaImageList, frxClass, frxDBSet, inifiles,Windows,
-  cxStyles;
+  SysUtils, Classes, mySQLDbTables, DB, sSkinManager, dialogs, forms, ImgList,
+  Controls, acAlphaImageList, frxClass, frxDBSet, inifiles, Windows, cxStyles,
+  SHFolder;
 
 type
   Tdm = class(TDataModule)
@@ -44,52 +44,61 @@ type
   private
     { Private declarations }
   public
-  kd_comp: string;
-    function FRObject(FastReport: TfrxReport; ObjectName: String): TObject;
-    function FRMemo (FastReport: TfrxReport; ObjectName: String): TfrxMemoView;
+    kd_comp: string;
+    function FRObject(FastReport: TfrxReport; ObjectName: string): TObject;
+    function FRMemo(FastReport: TfrxReport; ObjectName: string): TfrxMemoView;
     { Public declarations }
   end;
 
 var
   dm: Tdm;
-  WPath: String;
-  sop,metu_kabeh:boolean;
+  WPath, AppPath: string;
+  sop, metu_kabeh: boolean;
   PeriodAktif: string;
-  mgs:TMsg;
+  mgs: TMsg;
 
 implementation
 
 
 {$R *.dfm}
 
-function Tdm.FRObject(FastReport: TfrxReport; ObjectName: String): TObject;
+function GetAppData(Folder: Integer): string;
+var
+  path: array[0..MAX_PATH] of Char;
+begin
+  if Succeeded(SHGetFolderPath(0, Folder, 0, 0, @Path[0])) then
+    Result := path + '\Gain Profit\'
+  else
+    Result := '';
+end;
+
+function Tdm.FRObject(FastReport: TfrxReport; ObjectName: string): TObject;
 var
   i: Integer;
   AFound: Boolean;
 begin
-  AFound:=False;
+  AFound := False;
   for I := 0 to FastReport.ComponentCount - 1 do
   begin
     if LowerCase(FastReport.Components[i].Name) = LowerCase(ObjectName) then
     begin
-      Result:=TObject(FastReport.Components[i]);
-      AFound:=True;
+      Result := TObject(FastReport.Components[i]);
+      AFound := True;
       Break;
     end;
   end;
   if not AFound then
   begin
-    Result:=nil;
+    Result := nil;
   end;
 end;
 
-function Tdm.FRMemo (FastReport: TfrxReport; ObjectName: String): TfrxMemoView;
+function Tdm.FRMemo(FastReport: TfrxReport; ObjectName: string): TfrxMemoView;
 begin
   Result := TfrxMemoView(FRObject(FastReport, ObjectName));
 end;
 
-
-function krupuk(const s: String; CryptInt: Integer): String;
+function krupuk(const s: string; CryptInt: Integer): string;
 var
   i: integer;
   s2: string;
@@ -102,67 +111,73 @@ end;
 
 procedure Tdm.DataModuleCreate(Sender: TObject);
 var
-  data,pusat,jalur1,jalur2,nama,kata: string;
+  data, pusat, jalur1, jalur2, nama, kata: string;
   X: TextFile;
-  appINI : TIniFile;
+  appINI: TIniFile;
 begin
-My_conn.Connected:= False;
+  My_conn.Connected := False;
 
- assignfile(X,'tools\koneksi.cbCon');
- try
-   reset(X);
-   readln(X,pusat);
-   readln(X,data);
-   readln(X,jalur2);
-   readln(X,nama);
-   readln(X,kata);
-   closefile(X);
- my_conn.Host :=krupuk(pusat,6);
- my_conn.DatabaseName:= krupuk(data,6);
- jalur1 :=krupuk(jalur2,6);
- my_conn.Port :=strtoint(jalur1);
- my_conn.UserName :=krupuk(nama,6);
- my_conn.UserPassword :=krupuk(kata,6);
- my_conn.Connected:= true;
+  assignfile(X, 'tools\koneksi.cbCon');
+  try
+    reset(X);
+    readln(X, pusat);
+    readln(X, data);
+    readln(X, jalur2);
+    readln(X, nama);
+    readln(X, kata);
+    closefile(X);
+    my_conn.Host := krupuk(pusat, 6);
+    my_conn.DatabaseName := krupuk(data, 6);
+    jalur1 := krupuk(jalur2, 6);
+    my_conn.Port := strtoint(jalur1);
+    my_conn.UserName := krupuk(nama, 6);
+    my_conn.UserPassword := krupuk(kata, 6);
+    my_conn.Connected := true;
 
-except
-on E:exception do
-begin
- showmessage('koneksi tidak berhasil '#10#13''+e.Message);
- application.Terminate;
- exit;
-end;
-end;
+  except
+    on E: exception do
+    begin
+      showmessage('koneksi tidak berhasil '#10#13'' + e.Message);
+      application.Terminate;
+      exit;
+    end;
+  end;
 
-WPath := ExtractFilePath(Application.ExeName);
-sm.SkinDirectory:=wpath+'\tools\skins';
+  WPath := ExtractFilePath(Application.ExeName);
 
-  appINI := TIniFile.Create(WPath+'\tools\gain.ini') ;
- try
-  kd_comp := appINI.ReadString ('akun', 'kd_perusahaan', '');
-  sm.SkinName:=appINI.ReadString('akun','nama_skin','Acryl (internal)');
-  sm.HueOffset:=appini.ReadInteger('akun','hue_skin',0);
-  sm.Saturation:=appini.ReadInteger('akun','sat_skin',0);
+  AppPath := GetAppData(CSIDL_COMMON_APPDATA);
+  if not (DirectoryExists(AppPath)) then
+    CreateDir(AppPath);
+  sm.SkinDirectory := wpath + '\tools\skins';
 
- finally
- appINI.Free;
- end;
+  appINI := TIniFile.Create(WPath + '\tools\gain.ini');
+  try
+    kd_comp := appINI.ReadString('akun', 'kd_perusahaan', '');
+    sm.SkinName := appINI.ReadString('akun', 'nama_skin', 'Acryl (internal)');
+    sm.HueOffset := appini.ReadInteger('akun', 'hue_skin', 0);
+    sm.Saturation := appini.ReadInteger('akun', 'sat_skin', 0);
 
-sop:=true;
+  finally
+    appINI.Free;
+  end;
+
+  sop := true;
 
 end;
 
 procedure Tdm.smAfterChange(Sender: TObject);
-var  appINI : TIniFile;
+var
+  appINI: TIniFile;
 begin
-appINI := TIniFile.Create(WPath+'\tools\gain.ini') ;
- try
-  appINI.WriteString ('akun', 'nama_skin', dm.sm.SkinName);
-  appINI.WriteInteger ('akun', 'hue_skin', dm.sm.HueOffset);
-  appINI.WriteInteger ('akun', 'sat_skin', dm.sm.Saturation);
- finally
- appINI.Free;
- end;
+  appINI := TIniFile.Create(WPath + '\tools\gain.ini');
+  try
+    appINI.WriteString('akun', 'nama_skin', dm.sm.SkinName);
+    appINI.WriteInteger('akun', 'hue_skin', dm.sm.HueOffset);
+    appINI.WriteInteger('akun', 'sat_skin', dm.sm.Saturation);
+  finally
+    appINI.Free;
+  end;
 end;
 
 end.
+
