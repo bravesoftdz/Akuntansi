7a8c92a737f11fa9b05c8605288aa2f925d82e66
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-04-23 15:15:08 +0700

    project untuk delphi 7

--- delphi7/Accounting.dpr
+++ delphi7/Accounting.dpr
@@ -1,44 +1,40 @@
-program Accounting;
+program accounting;
 
 uses
   Forms,
-  u_cari in 'u_cari.pas' {f_cari},
-  u_dm in 'u_dm.pas' {dm: TDataModule},
-  UFungsi in '..\tools\pascal\UFungsi.pas',
-  u_GJurnal in 'u_GJurnal.pas' {F_GJurnal},
-  u_Jurnal_Kas in 'u_Jurnal_Kas.pas' {f_Jurnal_Kas},
-  u_utama in 'u_utama.pas' {f_utama},
-  u_klasifikasi_akun in 'u_klasifikasi_akun.pas' {f_klasifikasi_akun},
-  u_daftar_akun in 'u_daftar_akun.pas' {f_daftar_akun},
-  u_bayar_hutang in 'u_bayar_hutang.pas' {f_bayar_hutang},
-  u_bayar_piutang in 'u_bayar_piutang.pas' {f_bayar_piutang},
-  U_Login in 'U_Login.pas' {F_Login},
-  u_asset in 'u_asset.pas' {F_asset},
-  u_akun_penting in 'u_akun_penting.pas' {f_akun_penting},
-  u_daftar_asset in 'u_daftar_asset.pas' {f_daftar_asset},
-  u_daftar_jurnal_umum in 'u_daftar_jurnal_umum.pas' {f_daftar_jurnal_umum},
-  u_daftar_penjualan in 'u_daftar_penjualan.pas' {f_daftar_penjualan},
-  u_daftar_pembelian in 'u_daftar_pembelian.pas' {f_daftar_pembelian},
-  u_daftar_return in 'u_daftar_return.pas' {f_daftar_return},
-  u_daftar_return_kirim in 'u_daftar_return_kirim.pas' {f_daftar_return_kirim},
-  u_daftar_kirim in 'u_daftar_kirim.pas' {f_daftar_kirim},
-  u_daftar_koreksi in 'u_daftar_koreksi.pas' {f_daftar_koreksi},
-  u_daftar_hutang in 'u_daftar_hutang.pas' {f_daftar_hutang},
-  u_daftar_bayar_hutang in 'u_daftar_bayar_hutang.pas' {f_daftar_bayar_hutang},
-  u_daftar_bayar_piutang in 'u_daftar_bayar_piutang.pas' {f_daftar_bayar_piutang},
-  u_daftar_kas in 'u_daftar_kas.pas' {f_daftar_kas},
-  u_daftar_piutang in 'u_daftar_piutang.pas' {f_daftar_piutang},
-  uSaldoAwal in 'uSaldoAwal.pas' {FSaldoAwal};
+  u_akun_penting in '..\src\u_akun_penting.pas' {f_akun_penting},
+  u_asset in '..\src\u_asset.pas' {F_asset},
+  u_bayar_hutang in '..\src\u_bayar_hutang.pas' {f_bayar_hutang},
+  u_bayar_piutang in '..\src\u_bayar_piutang.pas' {f_bayar_piutang},
+  u_cari in '..\src\u_cari.pas' {f_cari},
+  u_daftar_akun in '..\src\u_daftar_akun.pas' {f_daftar_akun},
+  u_daftar_asset in '..\src\u_daftar_asset.pas' {f_daftar_asset},
+  u_daftar_bayar_hutang in '..\src\u_daftar_bayar_hutang.pas' {f_daftar_bayar_hutang},
+  u_daftar_bayar_piutang in '..\src\u_daftar_bayar_piutang.pas' {f_daftar_bayar_piutang},
+  u_daftar_hutang in '..\src\u_daftar_hutang.pas' {f_daftar_hutang},
+  u_daftar_jurnal_umum in '..\src\u_daftar_jurnal_umum.pas' {f_daftar_jurnal_umum},
+  u_daftar_kas in '..\src\u_daftar_kas.pas' {f_daftar_kas},
+  u_daftar_kirim in '..\src\u_daftar_kirim.pas' {f_daftar_kirim},
+  u_daftar_koreksi in '..\src\u_daftar_koreksi.pas' {f_daftar_koreksi},
+  u_daftar_pembelian in '..\src\u_daftar_pembelian.pas' {f_daftar_pembelian},
+  u_daftar_penjualan in '..\src\u_daftar_penjualan.pas' {f_daftar_penjualan},
+  u_daftar_piutang in '..\src\u_daftar_piutang.pas' {f_daftar_piutang},
+  u_daftar_return in '..\src\u_daftar_return.pas' {f_daftar_return},
+  u_daftar_return_kirim in '..\src\u_daftar_return_kirim.pas' {f_daftar_return_kirim},
+  u_dm in '..\src\u_dm.pas' {dm: TDataModule},
+  u_GJurnal in '..\src\u_GJurnal.pas' {F_GJurnal},
+  u_Jurnal_Kas in '..\src\u_Jurnal_Kas.pas' {f_Jurnal_Kas},
+  u_klasifikasi_akun in '..\src\u_klasifikasi_akun.pas' {f_klasifikasi_akun},
+  U_Login in '..\src\U_Login.pas' {F_Login},
+  u_utama in '..\src\u_utama.pas' {f_utama},
+  uSaldoAwal in '..\src\uSaldoAwal.pas' {FSaldoAwal},
+  UFungsi in '..\..\tools\pascal\UFungsi.pas';
 
 {$R *.res}
 
-
-
 begin
   Application.Initialize;
-  Application.Title := 'Account of Profit 2.0';
   Application.CreateForm(Tdm, dm);
   Application.CreateForm(Tf_utama, f_utama);
   Application.Run;
 end.
-
